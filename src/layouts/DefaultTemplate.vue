14a5c990c19be4a8ef8346c6855e34653b852e16
Diego Cardoso <diegocar448@hotmail.com>
2020-11-02 17:51:00 -0300

    router

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    asdsa
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {};
+</script>
+
+
+<style scoped>
+@import url("../assets/css/vuefood.css");
+@import url("../assets/css/reset.css");
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import DefaultTemplate from './layouts/DefaultTemplate';
+import router from './routes';
+
 
 Vue.config.productionTip = false
 
+
+
+
+
 new Vue({
-  render: h => h(App),
+  render: h => h(DefaultTemplate),
+  router
 }).$mount('#app')
