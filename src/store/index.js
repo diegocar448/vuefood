9f4c8d75a0718b40a4b2f37329433c36da4a2126
Diego Cardoso <diegocar448@hotmail.com>
2020-11-20 16:05:48 -0300

    modularizar o Vuex (companies)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import companies from "./modules/companies";
+
 /* 
 Assim o Vue usará o Vuex
 */
@@ -10,7 +12,9 @@ Vue.use(Vuex)
 Assim instaciamos o Vuex podendo tambem passar o parâmetros de configurações
 */
 const store = new Vuex.Store({
-
+    modules: {
+        companies
+    }
 });
 
 export default store
